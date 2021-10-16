91a90c746d5bcffd41e34421c2a2757ca5a7ecab
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2021-10-16 14:43:11 +0900

    Setting Box

--- momentum-clone/js/greeting.js
+++ momentum-clone/js/greeting.js
@@ -7,26 +7,33 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-if (savedUsername === null) {
-  // If saved username does not exist, loginForm should display
-  // Show the loginForm
-  loginForm.classList.remove(HIDDEN_CLASS_NAME);
+// When submit event occurs
+loginForm.addEventListener("submit", (submitEvent) => {
+  // Prevent refresh
+  submitEvent.preventDefault(); // Blocks default behaviors of the event
 
-  // When submit event occurs
-  loginForm.addEventListener("submit", (submitEvent) => {
-    // Prevent refresh
-    submitEvent.preventDefault(); // Blocks default behaviors of the event
-  
-    // Hide form and show greeting message
-    loginForm.classList.add(HIDDEN_CLASS_NAME); // Hide form
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username); // Save Username
-    greeting.innerText = `Hello, ${username}`;
-    greeting.classList.remove(HIDDEN_CLASS_NAME); // Show greting message
-  });
-} else {
-  // Display greeting message with saved username
-  greeting.innerText = `Hello ${savedUsername}`;
-  greeting.classList.remove(HIDDEN_CLASS_NAME);
+  // Hide form and show greeting message
+  loginForm.classList.add(HIDDEN_CLASS_NAME); // Hide form
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username); // Save Username
+  greeting.innerText = `Hello, ${username}`;
+  greeting.classList.remove(HIDDEN_CLASS_NAME); // Show greting message
+  loginInput.value = "";
+});
+
+function loadGreeting() {
+  const savedUsername = localStorage.getItem(USERNAME_KEY);
+  if (savedUsername === null) {
+    // If saved username does not exist, loginForm should display
+    // Show the loginForm
+    loginForm.classList.remove(HIDDEN_CLASS_NAME);
+    greeting.classList.add(HIDDEN_CLASS_NAME);
+  } else {
+    // Display greeting message with saved username
+    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASS_NAME);
+    loginForm.classList.add(HIDDEN_CLASS_NAME);
+  }
 }
+
+loadGreeting();
---
+++ momentum-clone/js/setup.js
@@ -0,0 +1,70 @@
+// Constant
+// Weather API --> using the API key defined in weather.js / "temp-unit"
+// greeting.js --> Use constants HIDDEN_CLASS_NAME and USERNAME_KEY
+const SELECTED_CLASS_NAME = "selected";
+
+// HTML Elements
+const setupIcon = document.querySelector("#setup-wrapper #setup-icon");
+const setupBox = document.querySelector("#setup-wrapper #setting-box");
+const changeUsernameButton = document.querySelector("#setup-wrapper #setting-box #change-username");
+const celciusButton = document.querySelector("#setup-wrapper #setting-box #temp-button #metric");
+const fahrenheitButton = document.querySelector("#setup-wrapper #setting-box #temp-button #imperial");
+
+// Color Unit by using saved preferrence
+let unit = localStorage.getItem("temp-unit");
+if(unit === null) {
+  unit = "metric";
+  localStorage.setItem("temp-unit", unit);
+}
+if(unit === "metric") {
+  celciusButton.classList.add(SELECTED_CLASS_NAME);
+} else {
+  fahrenheitButton.classList.add(SELECTED_CLASS_NAME);
+}
+
+// Setup icon clicked
+setupIcon.addEventListener("click", () => {
+  console.log("clicked");
+  if(setupBox.style.display === "flex") {
+    setupBox.style.display = "none";
+  } else {
+    setupBox.style.display = "flex";
+  }
+})
+
+// Change Username Button Clicked
+changeUsernameButton.addEventListener("click", () =>{
+  // Delete saved username
+  localStorage.removeItem(USERNAME_KEY);
+
+  // Call function to reload greeting;
+  loadGreeting(); // greeting.js
+});
+
+// Remove All Task Button Clicked
+
+// Change Unit
+celciusButton.addEventListener("click", () => {
+  // Reset the preference
+  unit = "metric";
+  localStorage.setItem("temp-unit", unit);
+
+  // Change display on setting panel
+  celciusButton.classList.add("selected");
+  fahrenheitButton.classList.remove("selected");
+
+  // Recall the weather API to get updated temperature
+  getWeather(); // defined in weather.js
+});
+fahrenheitButton.addEventListener("click", () => {
+  // Reset the preference
+  unit = "imperial";
+  localStorage.setItem("temp-unit", unit);
+
+  // Change display on setting panel
+  celciusButton.classList.remove(SELECTED_CLASS_NAME);
+  fahrenheitButton.classList.add(SELECTED_CLASS_NAME);
+
+  // Recall the weather API to get updated temperature
+  getWeather(); // defined in weather.js
+});
